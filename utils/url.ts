90bf3659ecab69c633a1f52f6ba3a887f09c33c4
Sahil3982 <sahilpatel3982@gmail.com>
2026-02-08 23:59:27 +0530

    url fixed

--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8001/v1/api/";
 console.log("BASE_URL",BASE_URL);
 
 export const endpoints = {
